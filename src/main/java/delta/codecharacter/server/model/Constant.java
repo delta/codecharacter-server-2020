5c61c314139f04120c01df3861b854c9e07fc994
Eswaraprasadp <peswar1701@gmail.com>
2020-01-08 18:46:22 +0530

    Add Notification model

---
+++ src/main/java/delta/codecharacter/server/model/Constant.java
@@ -0,0 +1,11 @@
+package delta.codecharacter.server.model;
+
+import javax.validation.constraints.NotNull;
+
+public class Constant {
+    @NotNull
+    private String key;
+
+    @NotNull
+    private String value;
+}
