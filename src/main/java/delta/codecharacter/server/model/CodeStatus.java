df0630cc384e8f5eebc3bfd7fc9e9440f357f782
Eswaraprasadp <peswar1701@gmail.com>
2020-01-08 18:58:13 +0530

    Add model CodeStatus

--- src/main/java/delta/codecharacter/server/model/CodeStatus.java
+++ src/main/java/delta/codecharacter/server/model/CodeStatus.java
@@ -1,9 +1,19 @@
 package delta.codecharacter.server.model;
 
+<<<<<<< HEAD
+=======
+import lombok.*;
+
+>>>>>>> Add model CodeStatus
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.util.Date;
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Builder
 public class CodeStatus {
     @NotNull
     @Positive
