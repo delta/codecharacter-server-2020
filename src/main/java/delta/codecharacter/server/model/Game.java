edfb21a82ac7ef8afb90c34e2b5a8cd972ea1853
Eswaraprasadp <peswar1701@gmail.com>
2020-01-08 19:10:49 +0530

    Add model Game

--- src/main/java/delta/codecharacter/server/model/Game.java
+++ src/main/java/delta/codecharacter/server/model/Game.java
@@ -1,10 +1,17 @@
 package delta.codecharacter.server.model;
 
+import lombok.*;
+
 import org.springframework.data.annotation.Id;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Builder
 public class Game {
     @Id
     @NotNull
