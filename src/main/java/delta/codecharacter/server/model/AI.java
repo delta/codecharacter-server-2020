533932fb95b474bc87bcc18963c6090dafc974ac
Eswaraprasadp <peswar1701@gmail.com>
2020-01-08 19:00:07 +0530

    Add model AI

--- src/main/java/delta/codecharacter/server/model/AI.java
+++ src/main/java/delta/codecharacter/server/model/AI.java
@@ -1,10 +1,19 @@
 package delta.codecharacter.server.model;
 
+<<<<<<< HEAD
+=======
+import lombok.*;
+>>>>>>> Add model AI
 import org.springframework.data.annotation.Id;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Builder
 public class AI {
     @Id
     @NotNull
