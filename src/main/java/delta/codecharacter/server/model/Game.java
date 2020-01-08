e21671fb812b4ac6fcb8185246dd6acad755ecb1
Eswaraprasadp <peswar1701@gmail.com>
2020-01-08 19:10:49 +0530

    Add model Game

---
+++ src/main/java/delta/codecharacter/server/model/Game.java
@@ -0,0 +1,36 @@
+package delta.codecharacter.server.model;
+
+import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+public class Game {
+    @Id
+    @NotNull
+    @Positive
+    private Integer id;
+
+    @NotNull
+    @Positive
+    private Integer matchId;
+
+    @NotNull
+    private String status;
+
+    @NotNull
+    private String verdict;
+
+    @NotNull
+    private Integer interestingness;
+
+    @NotNull
+    private Integer points1;
+
+    @NotNull
+    private Integer points2;
+
+    @NotNull
+    @Positive
+    private Integer mapId;
+}
