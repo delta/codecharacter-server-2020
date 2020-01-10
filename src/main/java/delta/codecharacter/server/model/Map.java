2802141b6fdca9b4b79481eef2058e3cd2bb2efc
Eswaraprasadp <peswar1701@gmail.com>
2020-01-10 18:42:44 +0530

    Add models Game, Match and Map

---
+++ src/main/java/delta/codecharacter/server/model/Map.java
@@ -0,0 +1,27 @@
+package delta.codecharacter.server.model;
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+/**
+ * A map is location where matches are held.
+ */
+@Data
+@Builder
+public class Map {
+    @NotNull
+    @Positive
+    private Integer id;
+
+    @Field("map_name")
+    @NotNull
+    private String mapName;
+
+    @Field("is_hidden")
+    @NotNull
+    @Builder.Default
+    private Boolean isHidden = false;
+}
---
+++ src/main/java/delta/codecharacter/server/util/Verdict.java
@@ -0,0 +1,5 @@
+package delta.codecharacter.server.util;
+
+public enum Verdict {
+    PLAYER_1, PLAYER_2, TIE
+}
