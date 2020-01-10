165ab8364b05aa1d853ceb9b0b5376cb8ef351a8
Eswaraprasadp <peswar1701@gmail.com>
2020-01-10 18:43:52 +0530

    Add models for Notification and Announcement

---
+++ src/main/java/delta/codecharacter/server/model/Announcement.java
@@ -0,0 +1,25 @@
+package delta.codecharacter.server.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Data
+@Builder
+public class Announcement {
+    @Id
+    @NotNull
+    @Positive
+    private Integer id;
+
+    @Field("admin_user_id")
+    @NotNull
+    @Positive
+    private Integer adminUserId;
+
+    @NotNull
+    private String message;
+}
---
+++ src/main/java/delta/codecharacter/server/model/Notification.java
@@ -0,0 +1,40 @@
+package delta.codecharacter.server.model;
+
+import delta.codecharacter.server.util.Type;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Data
+@Builder
+public class Notification {
+
+    @Id
+    @NotNull
+    @Positive
+    private Integer id;
+
+    @Field("user_id")
+    @NotNull
+    @Positive
+    private Integer userId;
+
+    @Field("is_read")
+    @NotNull
+    @Builder.Default
+    private Boolean isRead = false;
+
+    @NotBlank(message = "Title is mandatory")
+    private String title;
+
+    @NotBlank(message = "Content is mandatory")
+    private String content;
+
+    @NotBlank(message = "Type is mandatory")
+    private Type type;
+}
