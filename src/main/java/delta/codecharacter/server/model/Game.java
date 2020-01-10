ed32c3c8baf4f0b7ff278e816fb345bfa24aa469
Eswaraprasadp <peswar1701@gmail.com>
2020-01-10 18:42:44 +0530

    Add models Game, Match and Map

---
+++ src/main/java/delta/codecharacter/server/model/Game.java
@@ -0,0 +1,49 @@
+package delta.codecharacter.server.model;
+
+import delta.codecharacter.server.util.Status;
+import delta.codecharacter.server.util.Verdict;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Data
+@Builder
+public class Game {
+    @Id
+    @NotNull
+    @Positive
+    private Integer id;
+
+    @Field("match_id")
+    @NotNull
+    @Positive
+    private Integer matchId;
+
+    @NotNull
+    @Builder.Default
+    private Status status = Status.IDLE;
+
+    @NotNull
+    @Builder.Default
+    private Verdict verdict = Verdict.TIE;
+
+    @NotNull
+    @Builder.Default
+    private Integer interestingness = 0;
+
+    @Field("points_1")
+    @NotNull
+    private Integer points1;
+
+    @Field("points_2")
+    @NotNull
+    private Integer points2;
+
+    @Field("map_id")
+    @NotNull
+    @Positive
+    private Integer mapId;
+}
---
+++ src/main/java/delta/codecharacter/server/model/Match.java
@@ -0,0 +1,48 @@
+package delta.codecharacter.server.model;
+
+import delta.codecharacter.server.util.Status;
+import delta.codecharacter.server.util.Verdict;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+/**
+ * A match between two players. It may consist of more than one game.
+ */
+@Data
+@Builder
+public class Match {
+    @Id
+    @NotNull
+    @Positive
+    private Integer id;
+
+    @Field("player_id_1")
+    @NotNull
+    @Positive
+    private Integer playerId1;
+
+    @Field("player_id_2")
+    @NotNull
+    @Positive
+    private Integer playerId2;
+
+    @NotNull
+    @Builder.Default
+    private Verdict verdict = Verdict.TIE;
+
+    @NotNull
+    @Builder.Default
+    private Status status = Status.IDLE;
+
+    @Field("score_1")
+    @NotNull
+    private Integer score1;
+
+    @Field("score_2")
+    @NotNull
+    private Integer score2;
+}
