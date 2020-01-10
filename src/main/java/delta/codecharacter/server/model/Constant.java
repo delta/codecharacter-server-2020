5b4ee8d04d1244b407fae30399f38ffad138a74f
Eswaraprasadp <peswar1701@gmail.com>
2020-01-10 17:58:41 +0530

    Edit model Constant

--- src/main/java/delta/codecharacter/server/model/Constant.java
+++ src/main/java/delta/codecharacter/server/model/Constant.java
@@ -1,7 +1,14 @@
 package delta.codecharacter.server.model;
 
+import lombok.*;
+
 import javax.validation.constraints.NotNull;
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Builder
 public class Constant {
     @NotNull
     private String key;
