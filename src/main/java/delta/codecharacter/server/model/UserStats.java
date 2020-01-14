f5dbc8afcb02c179451e60844ba49dfa321656eb
cc19 <cc19@cc19.com>
2020-01-14 20:29:49 +0530

    Add model UserStats

---
+++ src/main/java/delta/codecharacter/server/model/UserStats.java
@@ -0,0 +1,32 @@
+package delta.codecharacter.server.model;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.util.Date;
+
+@Data
+@Builder
+public class UserStats {
+    @NotNull
+    @Positive
+    @Field("user_id")
+    private Integer userId;
+
+    @NotNull
+    @Field("total_wins")
+    private Integer totalWins;
+
+    @NotNull
+    @Field("total_loses")
+    private Integer totalLoses;
+
+
+    @Field("last_match_at")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private Date lastMatchAt;
+}
