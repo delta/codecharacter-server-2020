6855138bd56fabb37d4e1c5cda7a43548f909efc
mariovimal <mariovimal@gmail.com>
2020-02-28 13:06:36 +0530

    Fix response on Auth Success

--- src/main/java/delta/codecharacter/server/util/UserAuthUtil/CustomAuthenticationSuccessHandler.java
+++ src/main/java/delta/codecharacter/server/util/UserAuthUtil/CustomAuthenticationSuccessHandler.java
@@ -8,11 +8,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-        httpServletResponse.getWriter().write(new ObjectMapper().writeValueAsString("Authenticated"));
+        Map<String, String> res = new HashMap<String, String>();
+        res.put("message", "Authenticated");
+        res.put("status", "200");
+
+        httpServletResponse.getWriter().write(new ObjectMapper().writeValueAsString(res));
         httpServletResponse.setStatus(200);
     }
 }
