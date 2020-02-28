0721fa0af6bdbe621907fd240fd9b6b083a46a28
mariovimal <mariovimal@gmail.com>
2020-02-28 13:25:25 +0530

    Fix Cors config

--- src/main/java/delta/codecharacter/server/config/CorsFilter.java
+++ src/main/java/delta/codecharacter/server/config/CorsFilter.java
@@ -24,7 +24,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Authorization, Origin, Content-Type");
         response.setHeader("Access-Control-Expose-Headers", "X-Requested-With, Authorization, Origin, Content-Type");
 
         final HttpServletRequest request = (HttpServletRequest) req;
