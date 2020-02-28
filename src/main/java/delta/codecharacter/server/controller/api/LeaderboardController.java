5808d648baf4a7fa03f67a0092bfbf3f311dcd5f
mariovimal <mariovimal@gmail.com>
2020-02-28 13:10:29 +0530

    Fix .gitignore

--- src/main/java/delta/codecharacter/server/controller/api/LeaderboardController.java
+++ src/main/java/delta/codecharacter/server/controller/api/LeaderboardController.java
@@ -8,16 +8,14 @@ import delta.codecharacter.server.util.enums.UserType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
 import java.util.logging.Logger;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/leaderboard")
 public class LeaderboardController {
 
