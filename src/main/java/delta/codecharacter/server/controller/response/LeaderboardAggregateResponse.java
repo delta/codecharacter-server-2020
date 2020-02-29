1242fd230de16981162e8420d28334a3e59fe313
ElzaCS <anuejohn@gmail.com>
2020-02-29 14:13:11 +0530

    get match-stats and list of ratings also in all leaderboard routes

--- src/main/java/delta/codecharacter/server/controller/response/LeaderboardAggregateResponse.java
+++ src/main/java/delta/codecharacter/server/controller/response/LeaderboardAggregateResponse.java
@@ -1,28 +1,20 @@
-package delta.codecharacter.server.controller.response.Leaderboard;
+package delta.codecharacter.server.controller.response;
 
 import delta.codecharacter.server.util.enums.Division;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-import java.util.List;
-
 @Data
 @Builder
-public class LeaderboardData {
+public class LeaderboardAggregateResponse {
 
     @Field("user_id")
     private Integer userId;
 
     private String username;
 
-    private List<Integer> rating;
-
-    private Integer wins;
-
-    private Integer losses;
-
-    private Integer ties;
+    private Integer rating;
 
     private Division division;
 
