2b4968f7b41236617e1f14bb6364b50b779383d5
Eswaraprasadp <peswar1701@gmail.com>
2020-02-29 21:17:45 +0530

    Add created at for announcements

--- src/main/java/delta/codecharacter/server/model/Announcement.java
+++ src/main/java/delta/codecharacter/server/model/Announcement.java
@@ -1,11 +1,15 @@
 package delta.codecharacter.server.model;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Data
 @Builder
@@ -22,4 +26,9 @@ public class Announcement {
 
     @NotNull
     private String message;
+
+    @Field("created_at")
+    @NotNull
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.ofInstant(Instant.EPOCH, ZoneId.of("UTC"));
 }
