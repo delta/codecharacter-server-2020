e28e3c0e7410287756c799152ed7b25f4c884d78
Eswaraprasadp <peswar1701@gmail.com>
2020-02-29 18:23:06 +0530

    Change order of fields in PragyanuserDetails

--- src/main/java/delta/codecharacter/server/controller/response/PragyanUserDetails.java
+++ src/main/java/delta/codecharacter/server/controller/response/PragyanUserDetails.java
@@ -9,14 +9,14 @@ import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PragyanUserDetails {
-    @SerializedName("user_fullname")
-    private String fullName;
-
     @SerializedName("user_id")
     private Integer userId;
 
     @SerializedName("user_name")
     private String username;
+
+    @SerializedName("user_fullname")
+    private String fullName;
 
     @SerializedName("user_country")
     private String userCountry;
