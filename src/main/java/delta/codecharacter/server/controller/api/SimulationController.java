3afd668555002d2014ca220b280f4e8ed4b2a581
mariovimal <mariovimal@gmail.com>
2020-02-29 12:17:40 +0530

    Fix Simulation Controller

--- src/main/java/delta/codecharacter/server/controller/api/SimulationController.java
+++ src/main/java/delta/codecharacter/server/controller/api/SimulationController.java
@@ -5,18 +5,15 @@ import delta.codecharacter.server.model.User;
 import delta.codecharacter.server.service.SimulationService;
 import delta.codecharacter.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.logging.Logger;
 
-//@Controller
-@RestController
-@RequestMapping(value = "/simulate")
+@Controller
 public class SimulationController {
 
     private final Logger LOG = Logger.getLogger(SimulationController.class.getName());
@@ -27,8 +24,7 @@ public class SimulationController {
     @Autowired
     private UserService userService;
 
-    //    @MessageMapping("/match")
-    @PostMapping("/match")
+    @MessageMapping("/match")
     public void simulateMatch(@RequestBody @Valid SimulateMatchRequest simulateMatchRequest, Authentication authentication) {
         User user = userService.getUserByEmail(userService.getEmailFromAuthentication(authentication));
         simulationService.simulateMatch(simulateMatchRequest, user.getUserId());
