bb49d58b262801f14ec631a2f4c829e2428de4a8
Anu John <anuejohn@gmail.com>
2021-01-23 12:37:28 +0530

    Update source repository of storage directory (#78)

--- storage.sh
+++ storage.sh
@@ -1,5 +1,5 @@
 mkdir -p /home/code/Server
 mkdir -p /home/code/.m2
-git clone https://github.com/ElzaCS/codecharacter-storage.git storage
+git clone https://github.com/delta/codecharacter-storage-2021.git storage
 mv storage/storage /home/code/Server/storage
 rm -rf storage
