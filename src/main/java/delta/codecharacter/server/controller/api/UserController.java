9a03521969f985d69d225e9a9cabd056a2431d32
tharun571 <atharun05@gmail.com>
2021-01-23 18:52:02 +0530

    Add comment in updateLevel

--- src/main/java/delta/codecharacter/server/controller/api/UserController.java
+++ src/main/java/delta/codecharacter/server/controller/api/UserController.java
@@ -165,6 +165,7 @@ public class UserController {
         return new ResponseEntity<>(matchService.getManualAndAutoExecutedMatchesPaginated(user.getUserId(), pageable), HttpStatus.OK);
     }
 
+    // Update Quest level of the user by 1
     @PatchMapping(value = "/update-level")
     public ResponseEntity<Integer> updateLevel(Authentication authentication){
         String email = userService.getEmailFromAuthentication(authentication);
