6cc97ced6b6b9396b371d68ecfa254cefe8d05c4
PRADEEPERIYASAMY <58174391+PRADEEPERIYASAMY@users.noreply.github.com>
2021-01-23 22:52:40 +0530

    fix:remove repository function

--- src/main/java/delta/codecharacter/server/repository/LevelStatusRepository.java
+++ src/main/java/delta/codecharacter/server/repository/LevelStatusRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LevelStatusRepository extends MongoRepository<LevelStatus,Integer> {
-    LevelStatus findFirstByUserId (Integer userId);
 }
