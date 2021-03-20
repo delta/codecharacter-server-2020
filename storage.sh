48b7fb8edf90caef4bb68a94ccd7f0dc3f32d31a
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2021-03-21 02:11:26 +0530

    More robust storage script

--- storage.sh
+++ storage.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -d /tmp/codechar-storage ] ; then
+    rm -rf /tmp/codechar-storage
+fi
+
 git clone git@github.com:delta/codecharacter-storage-2021.git /tmp/codechar-storage
 rm -rf /tmp/codechar-storage/.git
-cp -r /tmp/codechar-storage/* ./storage
+
+if [ ! -d ./storage ] ; then
+    mkdir ./storage
+fi
+cp -r /tmp/codechar-storage/storage/* ./storage
