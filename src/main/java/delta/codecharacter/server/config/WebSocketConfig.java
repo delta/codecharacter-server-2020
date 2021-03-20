9555a8487a8183c61d5cc971de2cd7a3f109efc3
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2021-03-21 01:48:35 +0530

    Restrict allowed origin due to spring-projects/spring-framework#26111

--- src/main/java/delta/codecharacter/server/config/WebSocketConfig.java
+++ src/main/java/delta/codecharacter/server/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package delta.codecharacter.server.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +10,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Value("${client.request.origin}")
+    private String origin;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -22,7 +26,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // End-Point for the socket to connect.
-        registry.addEndpoint("/socket/connect").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/socket/connect").setAllowedOrigins(origin).withSockJS();
     }
 
 }
