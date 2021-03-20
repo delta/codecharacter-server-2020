eb903efe18b6de04ee31b5739f05f8e27deaa568
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2021-03-21 01:07:59 +0530

    Remove old docker config

--- docker/mongo-init.js
+++
@@ -1,5 +0,0 @@
-db.constant.insert([
-  { "key": "INITIAL_RATING", "value": "1200" },
-  { "key": "MATCH_WAIT_TIME", "value": 5 },
-  { "key": "AUTO_MATCH_UP_FREQUENCY_SECONDS", "value": "60" }
-])
--- docker/mongo.dockerfile
+++
@@ -1,4 +0,0 @@
-FROM mongo:latest
-COPY ./docker/mongo-init.js /docker-entrypoint-initdb.d/
-RUN mkdir -p /home/mongodb && touch /home/mongodb/.dbshell \
-&& chown -R 999:999 ./home/mongodb
