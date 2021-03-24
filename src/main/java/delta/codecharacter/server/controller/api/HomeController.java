1297dcc55d55848361cbe1386ef3c42b486af72d
ElzaCS <anuejohn@gmail.com>
2021-03-24 22:40:03 +0530

    Return static view in HomeService

--- src/main/java/delta/codecharacter/server/controller/api/HomeController.java
+++ src/main/java/delta/codecharacter/server/controller/api/HomeController.java
@@ -1,6 +1,7 @@
 package delta.codecharacter.server.controller.api;
 
 import delta.codecharacter.server.model.User;
+import delta.codecharacter.server.service.HomeService;
 import delta.codecharacter.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,18 +11,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Home {
+public class HomeController {
 
     @Autowired
     UserService userService;
 
+    @Autowired
+    HomeService homeService;
+
     @GetMapping(value = "/")
     public String getHomeString(Authentication authentication) {
-        User user = userService.getUserByEmail(userService.getEmailFromAuthentication(authentication));
-        if (user == null)
+        if (authentication == null)
             return "Invalid Login";
-        return "activation";
-//        return new ResponseEntity<>("Hello", HttpStatus.OK);
-
+        return homeService.getHomeString();
     }
 }
---
+++ src/main/java/delta/codecharacter/server/service/HomeService.java
@@ -0,0 +1,11 @@
+package delta.codecharacter.server.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeService {
+
+    public String getHomeString() {
+        return "activation";
+    }
+}
