a60ab4f22df8773a57b2bcc994c2783b23affa69
ElzaCS <anuejohn@gmail.com>
2021-03-25 14:23:28 +0530

    Refactor successfulLogin page

--- src/main/java/delta/codecharacter/server/controller/api/HomeController.java
+++ src/main/java/delta/codecharacter/server/controller/api/HomeController.java
@@ -1,20 +1,13 @@
 package delta.codecharacter.server.controller.api;
 
-import delta.codecharacter.server.model.User;
 import delta.codecharacter.server.service.HomeService;
-import delta.codecharacter.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
-    @Autowired
-    UserService userService;
 
     @Autowired
     HomeService homeService;
--- src/main/java/delta/codecharacter/server/service/HomeService.java
+++ src/main/java/delta/codecharacter/server/service/HomeService.java
@@ -1,11 +1,12 @@
 package delta.codecharacter.server.service;
 
+import delta.codecharacter.server.util.enums.Page;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HomeService {
 
     public String getHomeString() {
-        return "activation";
+        return String.valueOf(Page.successfulLogin);
     }
 }
